02a0f2c597147dea2870b0c97f2954ab5730156e
Renata <renata.ufop@yahoo.com.br>
2019-07-13 16:23:34 -0300

    primeiro commit

---
+++ src/main/java/br/com/desafio/capgemini/renata/DesafioCapgeminiApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.capgemini.renata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCapgeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCapgeminiApplication.class, args);
+	}
+
+}
